6c23590fa9e468275611db1d8340785284a5ead0
Gagan Bhat <gaganbhat6@gmail.com>
2019-08-29 19:25:18 -0500

    Remove junk and update jitpack and folder structure

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+
+public class Drivetrain extends SendableBase {
+
+
+
+	@Override
+	public void initSendable(SendableBuilder builder) {
+
+	}
+}
